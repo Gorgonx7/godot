ab3dd997500442bb990503c880914f0fc278b2ae
james <james.gordon1628@gmail.com>
2021-04-23 09:01:02 +0100

    added more tests

--- methods_test.py
+++ methods_test.py
@@ -1,9 +1,42 @@
-import unittest
+#!/usr/bin/env python
 
+import unittest
 import methods
+from SCons.Script import Dir, Environment
 
 
 class test_add_source_files(unittest.TestCase):
     def test_returns_encoded_files(self):
-        methods.add_source_file(["./main/main.cpp"], "*.cpp")
-        self.assertEqual()
+         
+        self.assertEqual(False, False)
+
+class test_using_clang(unittest.TestCase):
+    def test_returns_true(self):
+        env = {"CC" : "clang"}
+        self.assertEqual(methods.using_clang(env), True)
+    def test_returns_false(self):
+        env = {"CC" : "gcc"}
+        self.assertEqual(methods.using_clang(env), False)
+
+class test_using_gcc(unittest.TestCase):
+    def test_returns_true(self):
+        env = {"CC" : "gcc"}
+        self.assertEqual(methods.using_gcc(env), True)
+    def test_returns_false(self):
+        env = {"CC" : "clang"}
+        self.assertEqual(methods.using_gcc(env), False)
+
+class test_disable_warnings(unittest.TestCase):
+    def test_msvc_w_flag_added(self):
+        env_base = Environment()
+        env_base.msvc = True
+        env_base.__class__.disable_warnings = methods.disable_warnings
+        env_base.disable_warnings()
+        self.assertTrue(env_base.msvc)
+        print(env_base["CCFLAGS"])
+        self.assertTrue("/w" in env_base["CCFLAGS"])
+    def test_msvc_warn_flags(self):
+        env_base = Environment()
+        env_base.msvc = True
+        
+        
